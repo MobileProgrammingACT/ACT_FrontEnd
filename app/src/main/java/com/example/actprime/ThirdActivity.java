2f5353488c564e104c9e905d9a382656e33ad5ec
yebinlee <nqn4iwin@gmail.com>
2022-11-17 05:31:07 +0900

    18th commit

--- app/src/main/java/com/example/actprime/ThirdActivity.java
+++ app/src/main/java/com/example/actprime/ThirdActivity.java
@@ -1,45 +1,59 @@
 package com.example.actprime;
 
 import android.app.AlertDialog;
+import android.content.SharedPreferences;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ViewFlipper;
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import android.widget.EditText;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 
 public class ThirdActivity extends AppCompatActivity {
 
-    Button explain1,btnNext, btnPrev, btnEnd, submit;
+    /*DB 저장 정의용 - 위치 변경 X!!!*/
+    private FirebaseDatabase db = FirebaseDatabase.getInstance();
+    private DatabaseReference ref = db.getReference();
+
+    Button explain1, btnNext, btnPrev, btnEnd, submit;
     ImageButton musicButton;
     View activity1ExView;
     EditText content;
     ViewFlipper vFlipper;
     MediaPlayer musicPlayer;
+    String shared = "file"; //Edittext 값 저장용
     int count = 0;
-
-    DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
-    DatabaseReference conditionRef = mRootRef.child("text");
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.week_one_activity1);
+
+        /**/
+        submit = findViewById(R.id.submit);
+        content = findViewById(R.id.content);
+
+        /*저장 메소드 - writereview불러오기*/
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                writereview(content.getText().toString());
+                Toast.makeText(ThirdActivity.this, "저장했습니다", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        /*Edittext 값 유지 메소드*/
+        SharedPreferences sharedPreferences = getSharedPreferences(shared, 0);
+        String value = sharedPreferences.getString("key","");
+        content.setText(value);
 
         musicPlayer = MediaPlayer.create(this, R.raw.music1);
         musicPlayer.start();
@@ -57,9 +71,6 @@ public class ThirdActivity extends AppCompatActivity {
                 }
             }
         });
-
-        submit = (Button) findViewById(R.id.submit);
-        content = (EditText) findViewById(R.id.content);
 
         /*화면에 진입했을 때 활동에 관한 설명이 바로 뜨게하기 */
         activity1ExView = (View) View.inflate(ThirdActivity.this, R.layout.week_one_activity1_explanation, null);
@@ -183,20 +194,26 @@ public class ThirdActivity extends AppCompatActivity {
                 });
             }
         });
+
     }
 
+    /*Edittext 값 외부로 나간 이후에도 저장가능한 메소드*/
     @Override
-    protected void onStart() {
-        super.onStart();
-
-        submit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                conditionRef.setValue(content.getText().toString());
-                Toast.makeText(getApplicationContext(), "저장됐습니다", Toast.LENGTH_SHORT).show();
-            }
-        });
+    protected void onDestroy() {
+        super.onDestroy();
+        SharedPreferences  sharedPreferences = getSharedPreferences(shared, 0);
+        SharedPreferences.Editor editor  = sharedPreferences.edit();
+        String value = content.getText().toString();
+        editor.putString("key", value);
+        editor.commit();
     }
+
+    /*작성하는 메소드 writereview 정의, DB에 저장되는 방식 "review"키 값 내에 저장하기*/
+    public void writereview(String content) {
+        WriteReview writereview = new WriteReview(content);
+        ref.child("review").setValue(content);
+    }
+
     /*활동에서 나가면 음악이 꺼지게*/
     @Override
     protected void onStop() {
