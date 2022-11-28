f0ead9e5a14c308cdccde593b4e3b26a4d6d9e5e
yebinlee <nqn4iwin@gmail.com>
2022-11-28 12:54:28 +0900

    25th commit - 캘린더 추가

--- app/src/main/java/com/example/actprime/SignupActivity.java
+++ app/src/main/java/com/example/actprime/SignupActivity.java
@@ -6,8 +6,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CalendarView;
 import android.widget.EditText;
 import android.content.Intent;
 import android.widget.TextView;
@@ -18,13 +21,21 @@ import com.google.android.gms.tasks.Task;
 
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Date;
 
 public class SignupActivity extends AppCompatActivity {
 
     private FirebaseAuth firebaseAuth = null;
-    private EditText join_email, join_pwd;
+    private FirebaseDatabase firebaseDB = FirebaseDatabase.getInstance();
+    private DatabaseReference firebaseRef = firebaseDB.getReference();
+    public EditText new_email, new_pwd;
     public Button SignupBtn;
+
     View dialogView;
+    String text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,22 +44,25 @@ public class SignupActivity extends AppCompatActivity {
         setTitle("이메일 회원가입");
 
         SignupBtn = (Button) findViewById(R.id.SignupBtn);
-        join_email = (EditText) findViewById(R.id.join_email);
-        join_pwd = (EditText) findViewById(R.id.join_pwd);
+        new_email = (EditText) findViewById(R.id.join_email);
+        new_pwd = (EditText) findViewById(R.id.join_pwd);
+
+        text = new_email.getText().toString();
 
         firebaseAuth = FirebaseAuth.getInstance();
 
         SignupBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (!join_email.getText().toString().equals("") && !join_pwd.getText().toString().equals("")) {
-                    createUser(join_email.getText().toString(), join_pwd.getText().toString());
+                if (!new_email.getText().toString().equals("") && !new_pwd.getText().toString().equals("")) {
+                    createUser(new_email.getText().toString(), new_pwd.getText().toString());
                 } else {
                     Toast.makeText(SignupActivity.this, "계정과 비밀번호를 입력하세요.", Toast.LENGTH_SHORT).show();
                 }
             }
         });
     }
+
         private void createUser(String email, String pwd) {
             firebaseAuth.createUserWithEmailAndPassword(email, pwd)
                     .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
@@ -56,8 +70,7 @@ public class SignupActivity extends AppCompatActivity {
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             if (task.isSuccessful()) {
                                 // 회원가입 성공시
-                                Toast.makeText(SignupActivity.this, "회원가입 성공", Toast.LENGTH_SHORT).show();
-                                finish();
+                                Toast.makeText(SignupActivity.this, "회원가입 성공!" + text + "님 안녕하세요.", Toast.LENGTH_LONG).show();
                             } else {
                                 // 계정이 중복된 경우
                                 Toast.makeText(SignupActivity.this, "이미 존재하는 계정입니다.", Toast.LENGTH_SHORT).show();
@@ -65,13 +78,24 @@ public class SignupActivity extends AppCompatActivity {
                         }
                     });
 
+
             dialogView = (View) View.inflate(SignupActivity.this, R.layout.dialog, null);
+
             AlertDialog.Builder dlg = new AlertDialog.Builder(SignupActivity.this);
             dlg.setView(dialogView);
+
             dlg.setPositiveButton("확인", new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialogInterface, int i) {
-                    Intent main = new Intent(getApplicationContext(), MainActivity.class); //이거 로그인 페이지 뜨게 하기
+                    Intent main = new Intent(getApplicationContext(), MainActivity.class);
+                    LayoutInflater inflater = getLayoutInflater();
+                    View layout = inflater.inflate(R.layout.dialog, (ViewGroup) findViewById(R.id.dialog));
+                    CalendarView DateSelect;
+                    DateSelect = layout.findViewById(R.id.DateSelect);
+                    Date startdate = new Date(DateSelect.getDate());
+                    Date enddate = new Date(DateSelect.getDate() + 7);
+                    firebaseRef.child(text).child("startdate").setValue(startdate);
+                    firebaseRef.child(text).child("enddate").setValue(enddate);
                     startActivity(main);
                 }
             });
@@ -83,4 +107,5 @@ public class SignupActivity extends AppCompatActivity {
             });
             dlg.show();
             }
-        }
+
+}
